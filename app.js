3351984f20a92e7bb7fcad3fb5c85f84cb551557
hernangit config --global user.name John <hernan0066@gmail.com>
2022-05-08 13:46:41 -0300

    Borrar tareas

--- app.js
+++ app.js
@@ -46,9 +46,20 @@ const main = async () => {
       case "4": // listar pendientes
         tareas.listarPendientesCompletadas(false);
         break;
+
+      case "6": // Borrar
+        const id = await listadoTareasBorrar(tareas.listadoArr);
+        if (id !== "0") {
+          const ok = await confirmar("¿Está seguro?");
+          if (ok) {
+            tareas.borrarTarea(id);
+            console.log("Tarea borrada");
+          }
+        }
+        break;
     }
 
-    //guardarDB(tareas.listadoArr);
+    guardarDB(tareas.listadoArr);
     await pausa();
   } while (opt !== "0");
 };
